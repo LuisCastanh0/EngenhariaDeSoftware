fd57f93051e972a27e967aea701c37e5fdd136d7
Julian Teixeira <144359181+Julian-CT@users.noreply.github.com>
2024-05-27 08:31:29 -0300

    Update OwnerCtl.java

--- PartyHub/src/Control/OwnerCtl.java
+++ PartyHub/src/Control/OwnerCtl.java
@@ -7,10 +7,12 @@ public class OwnerCtl {
     // Lista de Users
     private ArrayList<Owner> Owners;
 
+    // Construtor
     public OwnerCtl() {
         Owners = new ArrayList<>();
     }
 
+    // Encontra um Owner pelo nome
     public Owner findOwner(String name) {
         for (Owner owner : Owners) {
             if (owner.getName().equals(name)) {
@@ -20,10 +22,12 @@ public class OwnerCtl {
         return null;
     }
 
+    // Adiciona um Owner ao sistema
     public void addOwner(Owner owner) {
         Owners.add(owner);
     }
 
+    // Verifica se o login é válido
     public boolean verifyLogin(Owner owner, String password) {
         if (owner.getPassword().equals(password)) {
             return true;
